2daf661f2d2a0751e549f6b2f7984b1322f10c21
Joyce Bosco <joycegodinhobosco@gmail.com>
2022-01-15 18:10:50 -0300

    mutation delete note implemented

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
 
     field :create_note, mutation: Mutations::CreateNote
-    
+    field :delete_note, mutation: Mutations::DeleteNote
+
   end
 end
